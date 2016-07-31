247f396d8e51340637e7d6dadb3ff925d4ee8b9e
Jesus Alkorta <jesus.alkorta@gmail.com>
2016-07-31 09:50:12 +0200

    added basic data sctructures and classes for containing and as simulation managers

--- gpsgen.py
+++ gpsgen.py
@@ -2,13 +2,12 @@ from itertools import cycle, count
 import datetime
 from collections import namedtuple
 
-Coord = namedtuple("Coord", ["lat","lon"])
 
 CARRIERS = 600
-AVG_SPEED = 13.8 # m/s
+AVG_SPEED = 13.8 # m/s +- 50 km/h
 START_TIME = datetime.datetime.now()
 
-
+Coord = namedtuple("Coord", ["lat","lon"])
 
 CITIES = [
         {
@@ -27,12 +26,99 @@ states = cycle(['free','to_provider','to_customer'])
 
 worker_sequence = count(start=1, step=1)
 
+task_sequence = count(start=1, step=1)
+
 def next_state():
     return next(states)
 
 def next_worker_id():
     return next(worker_sequence)
 
+def next_task_id():
+    return next(task_sequence)
+
 def get_current_day():
     today = datetime.date.today()
     return "{}{:02d}{:02d}".format(today.year,today.month,today.day)
+
+
+
+"""
+Not sure about this
+could be interesting, using it as
+>>> task = Task(next_task_id())
+Task = namedtuple("Task", ['task_id'])
+"""
+
+class Task():
+    def __init__(self):
+        self.task_id = next_task_id()
+
+
+class Worker():
+    def __init__(self, worker_id, city, coord, task_id, state='free'):
+        """In this proof of concept every Worker has a ACTION linked even if it is
+        <Free>, as well as the a last coord, gps coordinates for displaying 
+        the last valid position for the worker.
+        Speed is a vector np array it allow us to multiply matrixes"""
+        self.worker_id = worker_id
+        self.coord = coord
+        self.city = city
+        self.state = state
+        self.task_id = task_id
+        self.speed = np.array((0,0))
+
+
+    def _get_id(self):
+        return self.worker_id
+
+    def full_qualified_id():
+        """This is the full qualified id, that wwe will use as key on
+        redis(GEOADD)."""
+        return "{city}:{worker_id}:{task_id}"
+
+    def switch_state(self):
+        self.state = next_state()
+
+    def get_position(self):
+        return self.coord
+
+
+class Step():
+    def __init__(self, worker):
+        self.worker = worker
+
+    def to_dict(self):
+        return vars(self.worker)
+
+
+def gen_result(step):
+    """Returns a list with the complete simulation"""
+    pass
+
+def gen_next_step(step):
+    """Return one step, for streaming or whatever"""
+    pass
+
+
+class Simulation():
+    """This wasnt to be a base class for different simulation modes
+    like ToJsonFileSimulation or RealtimeSimulation (ok real time is always an
+    illusion)
+    We also add some metadata to the simulation like start ans end datetime"""
+    def __init__(self):
+        self.starts = datetime.datetime.now()
+        self.ends = None
+        self.steps = []
+
+class ToJsonFileSimulation(Simulation):
+
+    def add_step(self, step):
+        self.steps.append(step)
+
+
+class RealtimeSimulation(Simulation):
+
+    def gen_stream(self):
+        pass
+
